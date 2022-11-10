b756e19f5e705722abd0513825439bcac239ef3b
devminoh <gmerit0116@naver.com>
2022-11-10 16:27:40 +0900

    :art: style: login css 변경 #14

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,23 @@
-import './App.css';
 import GlobalStyles from './GlobalStyles';
+import styled from 'styled-components';
 import Header from './components/headers/Header'
+import Footer from './components/Footer';
+import Login from './pages/Login';
+
+const Main = styled.div`
+  display: flex;
+  justify-content: center;
+`;
 
 function App() {
   return (
     <>
     <GlobalStyles />
     <Header/>
+    <Main>
+      <Login />
+    </Main>
+    <Footer />
     </>
 
   );
