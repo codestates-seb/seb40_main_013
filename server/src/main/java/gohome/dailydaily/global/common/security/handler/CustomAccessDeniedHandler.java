e352325737f9d34451e5b7e4be88063babce9910
kkg5 <kkg05102@gmail.com>
2022-11-11 13:38:26 +0900

    :sparkles: feat: Security Handler 작성 - CustomAccessDeniedHandler - CustomAuthenticationEntryPoint - ErrorResponder

---
+++ server/src/main/java/gohome/dailydaily/global/common/security/handler/CustomAccessDeniedHandler.java
@@ -0,0 +1,21 @@
+package gohome.dailydaily.global.common.security.handler;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.web.access.AccessDeniedHandler;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Slf4j
+@Component
+public class CustomAccessDeniedHandler implements AccessDeniedHandler {
+    @Override
+    public void handle(HttpServletRequest request, HttpServletResponse response, AccessDeniedException accessDeniedException) throws IOException, ServletException {
+        log.warn("{}\nAccess Denied: {}", request.getPathInfo(), accessDeniedException.getMessage());
+        ErrorResponder.sendErrorResponse(response, response.SC_FORBIDDEN, "Access Denied");
+    }
+}
---
+++ server/src/main/java/gohome/dailydaily/global/common/security/handler/CustomAuthenticationEntryPoint.java
@@ -0,0 +1,21 @@
+package gohome.dailydaily.global.common.security.handler;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Slf4j
+@Component
+public class CustomAuthenticationEntryPoint implements AuthenticationEntryPoint {
+    @Override
+    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
+        log.warn("{}\nUnauthorized: {}", request.getPathInfo(), authException.getMessage());
+        ErrorResponder.sendErrorResponse(response, response.SC_UNAUTHORIZED, "Unauthorized");
+    }
+}
