b8354af1677f08a1b22cff3f46d64ca4e182c2fc
insidelamp <rkdhfh@naver.com>
2022-11-11 15:47:38 +0900

    :sparkles: feat: 상품 상세페이지  구조 작업 #22

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
-import GlobalStyles from './GlobalStyles';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import styled from 'styled-components';
-import Header from './components/headers/Header'
-import Footer from './components/Footer';
-import Login from './pages/Login';
-import Main from './pages/Main';
-import MyPage from './pages/Mypage';
+import GlobalStyles from "./GlobalStyles";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import styled from "styled-components";
+import Header from "./components/headers/Header";
+import Footer from "./components/Footer";
+import Login from "./pages/Login";
+import Main from "./pages/Main";
+import MyPage from "./pages/Mypage";
+import Signup from "./pages/Signup";
+import ArticleDetail from "./pages/detail/ArticleDetail";
 
 const MainContainter = styled.div`
   display: flex;
@@ -16,13 +18,15 @@ function App() {
   return (
     <BrowserRouter>
       <GlobalStyles />
-        <div className="App">
-        <Header/>
+      <div className="App">
+        <Header />
         <MainContainter>
           <Routes>
             <Route path="/" element={<Main />} />
             <Route path="/users/login" element={<Login />} />
             <Route path="/users/me/*" element={<MyPage />} />
+            <Route path="/signup" element={<Signup />} />
+            <Route path="/detail/:id" element={<ArticleDetail />} />
           </Routes>
         </MainContainter>
         <Footer />
