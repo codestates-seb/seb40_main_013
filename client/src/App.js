9b693bc6679b9605439bad8a1a1ad753ba782be6
devminoh <gmerit0116@naver.com>
2022-11-11 14:36:59 +0900

    :sparkles: feat: 메인페이지뼈대구현 #21

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,33 @@
 import GlobalStyles from './GlobalStyles';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import styled from 'styled-components';
 import Header from './components/headers/Header'
 import Footer from './components/Footer';
 import Login from './pages/Login';
+import Main from './pages/Main';
+import MyPage from './pages/Mypage';
 
-const Main = styled.div`
+const MainContainter = styled.div`
   display: flex;
   justify-content: center;
 `;
 
 function App() {
   return (
-    <>
-    <GlobalStyles />
-    <Header/>
-    <Main>
-      <Login />
-    </Main>
-    <Footer />
-    </>
-
+    <BrowserRouter>
+      <GlobalStyles />
+        <div className="App">
+        <Header/>
+        <MainContainter>
+          <Routes>
+            <Route path="/" element={<Main />} />
+            <Route path="/users/login" element={<Login />} />
+            <Route path="/users/me/*" element={<MyPage />} />
+          </Routes>
+        </MainContainter>
+        <Footer />
+      </div>
+    </BrowserRouter>
   );
 }
 
