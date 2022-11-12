5b66d3238970645e5225267c1cf40327402321b3
kkg5 <kkg05102@gmail.com>
2022-11-12 18:10:44 +0900

    :sparkles: feat: MemberController > MemberId 애너테이션 적용

--- server/src/main/java/gohome/dailydaily/domain/member/controller/MemberController.java
+++ server/src/main/java/gohome/dailydaily/domain/member/controller/MemberController.java
@@ -4,6 +4,7 @@ import gohome.dailydaily.domain.member.dto.MemberDto;
 import gohome.dailydaily.domain.member.entity.Member;
 import gohome.dailydaily.domain.member.mapper.MemberMapper;
 import gohome.dailydaily.domain.member.service.MemberService;
+import gohome.dailydaily.global.common.security.resolver.MemberId;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.validation.annotation.Validated;
@@ -21,16 +22,15 @@ public class MemberController {
     private final MemberMapper memberMapper;
 
     @PatchMapping
-    public MemberDto.UserResponse patchMember(@Valid @RequestBody MemberDto.Patch patch) {
-        Long memberId = 1L; // 시큐리티 구현 필요
+    public MemberDto.UserResponse patchMember(@MemberId Long memberId,
+                                              @Valid @RequestBody MemberDto.Patch patch) {
         Member member = memberService.updateMember(memberMapper.toMember(patch, memberId));
         return memberMapper.toResponse(member);
     }
 
     @DeleteMapping
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void deleteMember() {
-        Long memberId = 1L; // 시큐리티 구현 필요
+    public void deleteMember(@MemberId Long memberId) {
         memberService.deleteMember(memberId);
     }
 
