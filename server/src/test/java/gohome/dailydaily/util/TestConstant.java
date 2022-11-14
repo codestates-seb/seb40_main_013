1e3c1deac6b4bdebae023bb6da4cd22e040630d3
kkg5 <kkg05102@gmail.com>
2022-11-14 15:53:33 +0900

    :white_check_mark: test: REQUEST_HEADER_JWT 추가, final 키워드 추가

--- server/src/test/java/gohome/dailydaily/util/TestConstant.java
+++ server/src/test/java/gohome/dailydaily/util/TestConstant.java
@@ -3,18 +3,21 @@ package gohome.dailydaily.util;
 import gohome.dailydaily.domain.member.entity.Member;
 import gohome.dailydaily.domain.member.entity.MemberRole;
 import gohome.dailydaily.domain.member.entity.MemberStatus;
+import org.springframework.restdocs.headers.RequestHeadersSnippet;
 import org.springframework.restdocs.operation.preprocess.OperationRequestPreprocessor;
 import org.springframework.restdocs.operation.preprocess.OperationResponsePreprocessor;
 import org.springframework.restdocs.payload.FieldDescriptor;
 import org.springframework.restdocs.payload.JsonFieldType;
 import org.springframework.restdocs.payload.ResponseFieldsSnippet;
 
+import static org.springframework.restdocs.headers.HeaderDocumentation.headerWithName;
+import static org.springframework.restdocs.headers.HeaderDocumentation.requestHeaders;
 import static org.springframework.restdocs.operation.preprocess.Preprocessors.*;
 import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
 import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
 
 public class TestConstant {
-    public static Member MEMBER = Member.builder()
+    public static final Member MEMBER = Member.builder()
             .id(1L)
             .email("test@test.com")
             .nickname("닉네임")
@@ -24,7 +27,7 @@ public class TestConstant {
             .memberStatus(MemberStatus.ACTIVE)
             .build();
 
-    public static OperationRequestPreprocessor REQUEST_PREPROCESSOR =
+    public static final OperationRequestPreprocessor REQUEST_PREPROCESSOR =
             preprocessRequest(
                     modifyUris()
                             .scheme("https")
@@ -32,17 +35,21 @@ public class TestConstant {
                             .removePort(),
                     prettyPrint()
             );
-    public static OperationResponsePreprocessor RESPONSE_PREPROCESSOR = preprocessResponse(prettyPrint());
+    public static final OperationResponsePreprocessor RESPONSE_PREPROCESSOR = preprocessResponse(prettyPrint());
 
-    public static FieldDescriptor FWP_MEMBER_ID = fieldWithPath("memberId").type(JsonFieldType.NUMBER).description("멤버 식별자");
-    public static FieldDescriptor FWP_EMAIL = fieldWithPath("email").type(JsonFieldType.STRING).description("이메일");
-    public static FieldDescriptor FWP_NICKNAME = fieldWithPath("nickname").type(JsonFieldType.STRING).description("닉네임");
-    public static FieldDescriptor FWP_ADDRESS = fieldWithPath("address").type(JsonFieldType.STRING).description("주소");
-    public static FieldDescriptor FWP_PHONE = fieldWithPath("phone").type(JsonFieldType.STRING).description("휴대폰 번호");
-    public static FieldDescriptor FWP_MEMBER_STATUS = fieldWithPath("memberStatus").type(JsonFieldType.STRING).description("멤버 상태");
-    public static FieldDescriptor FWP_PASSWORD = fieldWithPath("password").type(JsonFieldType.STRING).description("비밀번호");
+    public static final RequestHeadersSnippet REQUEST_HEADER_JWT = requestHeaders(
+            headerWithName("Authorization").description("JWT 토큰")
+    );
 
-    public static ResponseFieldsSnippet MEMBER_RESPONSE_FIELDS = responseFields(
+    public static final FieldDescriptor FWP_MEMBER_ID = fieldWithPath("memberId").type(JsonFieldType.NUMBER).description("멤버 식별자");
+    public static final FieldDescriptor FWP_EMAIL = fieldWithPath("email").type(JsonFieldType.STRING).description("이메일");
+    public static final FieldDescriptor FWP_NICKNAME = fieldWithPath("nickname").type(JsonFieldType.STRING).description("닉네임");
+    public static final FieldDescriptor FWP_ADDRESS = fieldWithPath("address").type(JsonFieldType.STRING).description("주소");
+    public static final FieldDescriptor FWP_PHONE = fieldWithPath("phone").type(JsonFieldType.STRING).description("휴대폰 번호");
+    public static final FieldDescriptor FWP_MEMBER_STATUS = fieldWithPath("memberStatus").type(JsonFieldType.STRING).description("멤버 상태");
+    public static final FieldDescriptor FWP_PASSWORD = fieldWithPath("password").type(JsonFieldType.STRING).description("비밀번호");
+
+    public static final ResponseFieldsSnippet MEMBER_RESPONSE_FIELDS = responseFields(
             FWP_MEMBER_ID, FWP_EMAIL, FWP_NICKNAME, FWP_ADDRESS, FWP_PHONE, FWP_MEMBER_STATUS
     );
 
