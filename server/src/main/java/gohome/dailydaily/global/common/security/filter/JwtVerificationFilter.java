51dd5fcb45d6e6e25b94a0543fe9ae08a5a80899
kkg5 <kkg05102@gmail.com>
2022-11-15 14:23:26 +0900

    :sparkles: feat: JWT 예외 처리

--- server/src/main/java/gohome/dailydaily/global/common/security/filter/JwtVerificationFilter.java
+++ server/src/main/java/gohome/dailydaily/global/common/security/filter/JwtVerificationFilter.java
@@ -2,12 +2,12 @@ package gohome.dailydaily.global.common.security.filter;
 
 import gohome.dailydaily.global.common.security.util.CustomAuthorityUtils;
 import gohome.dailydaily.global.common.security.util.JwtTokenizer;
+import io.jsonwebtoken.Claims;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import javax.servlet.FilterChain;
@@ -16,7 +16,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.List;
-import java.util.Map;
 
 @RequiredArgsConstructor
 public class JwtVerificationFilter extends OncePerRequestFilter {
@@ -26,8 +25,12 @@ public class JwtVerificationFilter extends OncePerRequestFilter {
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-        Map<String, Object> claims = verifyJws(request);
-        setAuthenticationToContext(claims);
+        try {
+            Claims claims = verifyJws(request);
+            setAuthenticationToContext(claims);
+        } catch (Exception exception) {
+            request.setAttribute("exception", exception);
+        }
 
         filterChain.doFilter(request, response);
     }
@@ -38,14 +41,14 @@ public class JwtVerificationFilter extends OncePerRequestFilter {
         return authorization == null || !authorization.startsWith("Bearer");
     }
 
-    private Map<String, Object> verifyJws(HttpServletRequest request) {
+    private Claims verifyJws(HttpServletRequest request) {
         String jws = request.getHeader("Authorization").replace("Bearer ", "");
         String base64EncodedSecretKey = jwtTokenizer.encodedBase64SecretKey(jwtTokenizer.getSecretKey());
         return jwtTokenizer.getClaims(jws, base64EncodedSecretKey).getBody();
     }
 
-    private void setAuthenticationToContext(Map<String, Object> claims) {
-        Long id = Long.parseLong((String) claims.get("sub"));
+    private void setAuthenticationToContext(Claims claims) {
+        Long id = Long.parseLong(claims.getSubject());
         List<GrantedAuthority> authorities = authorityUtils.createAuthorities((List<String>) claims.get("roles"));
         Authentication authentication = new UsernamePasswordAuthenticationToken(id, null, authorities);
         SecurityContextHolder.getContext().setAuthentication(authentication);
