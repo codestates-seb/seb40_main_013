13008387ca202c90e355aa6e19f90ee30014c0c9
inside_lamp <93029270+insidelamp@users.noreply.github.com>
2022-11-16 16:24:25 +0900

    [Client] 로그인, 푸터 반응형 css 구현 (#50)
    * :art: style: login css 변경 #14
    
    * :sparkles: feat: 메인페이지뼈대구현 #21
    
    * :sparkles: feat: 상품 상세페이지  구조 작업 #22
    
    * :sparkles: feat: 회원가입페이지 뼈대작업 #17
    
    * :sparkles: feat: login 페이지  뼈대 변경, Signup axios 기능추가, 리덕스툴킷 설치 및 구조작업
    
    * :sparkles: feat: login 기능작업 #14
    
    * :sparkles: feat: 로그인 데이터통신작업, 상세페이지 데이터통신작업 #14 #22
    
    * :sparkles: feat: 토큰저장추가 및 style 수정
    
    * :memo: docs: 경로 변경 및 폴더삭제
    
    * :sparkles: feat: refreshToken을 활용한 엑세스토큰 재갱신 완료
    
    * :lipstick: style: 반응형 css구현중 footer, login
    
    Co-authored-by: devminoh <gmerit0116@naver.com>

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,15 +1,15 @@
 import styled from "styled-components/macro";
-import logo from '../imgs/logo.png'
-import { Link } from 'react-router-dom';
-import { BsCart3, BsSearch } from 'react-icons/bs';
+import logo from "../imgs/logo.png";
+import { Link } from "react-router-dom";
+import { BsCart3, BsSearch } from "react-icons/bs";
 import { useState, useRef, useEffect } from "react";
-import DownSearch from './search'
+import DownSearch from "./search";
 
 const HeaderBlock = styled.header`
   width: 100%;
   height: 173px;
   color: var(--color-gray);
-  div{
+  div {
     display: flex;
   }
   .top {
@@ -25,7 +25,7 @@ const Logo = styled.div`
   height: 100px;
   justify-content: center;
   align-items: center;
-  div{
+  div {
     font-size: 43px;
     color: var(--color-navy);
   }
@@ -35,28 +35,26 @@ const Serach = styled.div`
   margin-right: 8px;
 `;
 
-
 const Category = styled.div`
-
- display: flex;
- justify-content: space-between;
- border-bottom: 1px solid #AAAAAA;
- padding: 7px;
- margin: 5px 30px 5px 30px;
- .space{
+  display: flex;
+  justify-content: space-between;
+  border-bottom: 1px solid #aaaaaa;
+  padding: 7px;
+  margin: 5px 30px 5px 30px;
+  .space {
     margin-right: 20px;
-  &:hover{
-    cursor: pointer;
+    &:hover {
+      cursor: pointer;
+    }
   }
-  }
-  .cart-count{
+  .cart-count {
     font-size: 12px;
   }
 `;
 
-const Nav =styled.nav`
+const Nav = styled.nav`
   background-color: white;
-  border: 1px solid #AAAAAA;
+  border: 1px solid #aaaaaa;
   width: 60px;
   display: flex;
   justify-content: space-between;
@@ -88,70 +86,74 @@ const PageMove = styled(Link)`
 
 function Header() {
   //const navigate = useNavigate();
-    const modalRef = useRef();
-    const [closeSearch, setCloseSearch] = useState(false)
-    
-    const closeHandler = () => {
-      setCloseSearch(!closeSearch);
-    }
+  const modalRef = useRef();
+  const [closeSearch, setCloseSearch] = useState(false);
 
-    const outModalCloseHandler = ({ target }) => {
-      if(closeSearch && !modalRef.current.contains(target)) setCloseSearch(false);
+  const closeHandler = () => {
+    setCloseSearch(!closeSearch);
+  };
+
+  const outModalCloseHandler = ({ target }) => {
+    if (closeSearch && !modalRef.current.contains(target))
+      setCloseSearch(false);
+  };
+
+  useEffect(() => {
+    window.addEventListener("click", outModalCloseHandler);
+    return () => {
+      window.removeEventListener("click", outModalCloseHandler); //이벤트 한번만 실행되게 하려고 제거.
     };
+  });
 
-    useEffect(() => {
-      window.addEventListener('click', outModalCloseHandler);
-      return () => {
-        window.removeEventListener('click', outModalCloseHandler); //이벤트 한번만 실행되게 하려고 제거.
-      }; 
-    });
+  return (
+    <>
+      <HeaderBlock>
+        <div className="top">
+          <Link to="/users/login">
+            <LoginBtn>로그인/회원가입</LoginBtn>
+          </Link>
+          <Link to="/users/me/*">
+            <LoginBtn>마이페이지</LoginBtn>
+          </Link>
+        </div>
+        <Link to="/">
+          <Logo>
+            <div>DAILY DAILY</div>
+          </Logo>
+        </Link>
+        <Category>
+          <div>
+            <Link to="/sub">
+              <div className="space">서재</div>
+            </Link>
+            <Link to="/cart">
+              <div className="space">침실</div>
+            </Link>
+            <div className="space">거실</div>
+            <div className="space">주방</div>
+          </div>
 
-    return (
-      <>
-        <HeaderBlock>
-            <div className="top">
-                <Link to="/users/login">
-                  <LoginBtn>로그인/회원가입</LoginBtn>
-                </Link>
-                <Link to="/users/me/*">
-                  <LoginBtn>마이페이지</LoginBtn>
-                </Link>
+          <div>
+            <div ref={modalRef} className="modal">
+              <Serach onClick={closeHandler}>
+                <BsSearch size="20" />
+              </Serach>
+              {closeSearch && (
+                <DownSearch
+                  closeSearch={closeSearch}
+                  closeHandler={closeHandler}
+                />
+              )}
             </div>
-              <Link to="/">
-                <Logo>
-                  <div>DAILY DAILY</div>
-                </Logo>
-              </Link>
-            <Category>
-                <div>
-                  <Link to='/sub'>
-                    <div className="space">서재</div>
-                  </Link>
-                  <Link to='/cart'>
-                    <div className="space">침실</div>
-                  </Link>
-                    <div className="space">거실</div>
-                    <div className="space">주방</div>
-                </div>
-                
-                <div>
-                    <div ref={modalRef} className='modal'>
-                      <Serach onClick={closeHandler}>
-                       <BsSearch size='20'/>
-                      </Serach>
-                      {closeSearch && 
-                       <DownSearch closeSearch={closeSearch} closeHandler={closeHandler} />
-                      }
-                    </div>
-                    <div>
-                        <BsCart3 size='20'/>
-                        <div className="cart-count">(0)</div>
-                    </div>
-                </div>
-            </Category>
-        </HeaderBlock>
-      </>
-    );
-  }
-  
-  export default Header;
+            <div>
+              <BsCart3 size="20" />
+              <div className="cart-count">(0)</div>
+            </div>
+          </div>
+        </Category>
+      </HeaderBlock>
+    </>
+  );
+}
+
+export default Header;
