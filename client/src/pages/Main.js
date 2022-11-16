4b3f45be32bfa16e3f351f4285d85a7b1d4a7950
chocholove <93895421+devminoh@users.noreply.github.com>
2022-11-16 18:17:33 +0900

    [Client] 메인페이지 뼈대 수정 (#54)
    * :art: style: login css 변경 #14
    
    * :sparkles: feat: 메인페이지뼈대구현 #21
    
    * :sparkles: feat: product component create #21
    
    * :sparkles: feat: mypage editprofile  뼈대구현 #29
    
    * :sparkles: feat: editprofile 뼈대수정 #29
    
    * :sparkles: feat: mypage 구매내역 뼈대 완성 #29
    
    * :sparkles: feat: carousel 수정 #21
    
    * :sparkles: feat: main add #21

--- client/src/pages/Main.js
+++ client/src/pages/Main.js
@@ -1,39 +1,39 @@
 import React, { useState, useEffect } from "react";
 //import { Link } from "react-router-dom";
 import styled from "styled-components/macro";
-import Carousel from "../components/mains/Calousel";
+import Carousel from "../components/mains/Calousel2";
 import Button from "../components/Button";
 import Products from "../components/mains/Product";
 import { Link } from "react-router-dom";
 import axios from "axios";
 import Apis from "../apis/apis";
+import BrandProducts from "../components/mains/BrandProducts";
 
 const Container = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
-  margin: 20px 0;
 `;
 const Title = styled.h2`
   display: flex;
-  font-weight: 700;
+  font-weight: 400;
   font-size: 2rem;
-  margin: 30px 0 10px 0;
-  color: #515151;
+  margin: 40px 0 10px 0;
+  color: var(--font-black);
 `;
 const Hr = styled.hr`
   height: 7px;
   width: 70px;
-  background-color: #515151;
-  margin-bottom: 20px;
-  color: #515151;
+  background-color: var(--font-black);
+  margin-bottom: 10px;
+  color: var(--font-black);
 `;
 const FullTitle = styled.div`
   display: flex;
   justify-content: flex-end;
   width: 80%;
   margin-bottom: 10px;
-  color: #515151;
+  color: #AAAAAA;
 `;
 const FullView = styled(Link)`
   font-size: 1.5rem;
@@ -43,60 +43,39 @@ const FullView = styled(Link)`
 `;
 const ProductList = styled.div`
   width: 80%;
-  margin-bottom: 20px;
+  margin: 30px 0;
   display: flex;
   justify-content: center;
 `;
 
 const Main = () => {
-  const [productList, setProductList] = useState([]);
+  const [productList, setProductList] = useState([]);  
 
   useEffect(() => {
-    Apis.get(`products`).then((data) => setProductList(data.data));
+    Apis.get(`products`).then((data) => 
+    setProductList(data.data));
   }, []);
-  console.log(productList);
-
   return (
     <Container>
       <Carousel />
-      <Title>거실</Title>
+      <Title>Best of Best</Title>
       <Hr />
       <FullTitle name="fullTitle" className="fullTitle">
-        <FullView to="/sub" name="fullView" className="fullView">
-          전체보기 &gt;&gt;
-        </FullView>
       </FullTitle>
       <ProductList>
-        {productList.map((product) => (
+        {productList.filter(product => product.id < 6).map(product =>
           <Products
             brand={product.brand}
             img={product.img}
             key={product.id}
-            name={product.name}
+            title={product.title}
             price={product.price}
-            star={product.star}
+            score={product.score}
           />
-        ))}
+        )}
       </ProductList>
-      <Title>서재</Title>
-      <Hr />
-      <FullTitle name="fullTitle" className="fullTitle">
-        <FullView to="/sub" name="fullView" className="fullView">
-          전체보기 &gt;&gt;
-        </FullView>
-      </FullTitle>
-      <ProductList>
-        {productList.map((product) => (
-          <Products
-            brand={product.brand}
-            img={product.img}
-            key={product.id}
-            name={product.name}
-            price={product.price}
-            star={product.star}
-          />
-        ))}
-      </ProductList>
+      <Title>브랜드별 추천상품</Title>
+      <BrandProducts productList={productList.filter(p => p.id < 6)}/>
       <Title>침실</Title>
       <Hr />
       <FullTitle name="fullTitle" className="fullTitle">
@@ -110,9 +89,9 @@ const Main = () => {
             brand={product.brand}
             img={product.img}
             key={product.id}
-            name={product.name}
+            title={product.title}
             price={product.price}
-            star={product.star}
+            score={product.score}
             colorChip={product.colorChip}
           />
         ))}
