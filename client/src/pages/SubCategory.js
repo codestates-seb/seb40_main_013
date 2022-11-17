8a04097ab9997a4b26e5660470c325e7580500e0
inside_lamp <93029270+insidelamp@users.noreply.github.com>
2022-11-17 17:34:50 +0900

    [Client] 회원가입 반응형 css 구현 피드백 대기중 (#60)
    * :art: style: login css 변경 #14
    
    * :sparkles: feat: 메인페이지뼈대구현 #21
    
    * :sparkles: feat: 상품 상세페이지  구조 작업 #22
    
    * :sparkles: feat: 회원가입페이지 뼈대작업 #17
    
    * :sparkles: feat: login 페이지  뼈대 변경, Signup axios 기능추가, 리덕스툴킷 설치 및 구조작업
    
    * :sparkles: feat: login 기능작업 #14
    
    * :sparkles: feat: 로그인 데이터통신작업, 상세페이지 데이터통신작업 #14 #22
    
    * :sparkles: feat: 토큰저장추가 및 style 수정
    
    * :memo: docs: 경로 변경 및 폴더삭제
    
    * :sparkles: feat: refreshToken을 활용한 엑세스토큰 재갱신 완료
    
    * :lipstick: style: 반응형 css구현중 footer, login
    
    * :sparkles: feat: detail 부분 통신 작업시작, 반응형 css 구현중
    
    * :lipstick: style: 회원가입 반응형 css 완료 피드백 대기중
    
    Co-authored-by: devminoh <gmerit0116@naver.com>

--- client/src/pages/SubCategory.js
+++ client/src/pages/SubCategory.js
@@ -1,57 +1,57 @@
 import { useState, useEffect } from "react";
 import styled from "styled-components/macro";
-import SubCarousel from '../components/subcategories/SubCalousel'
+import SubCarousel from "../components/subcategories/SubCalousel";
 import Apis from "../apis/apis";
 import Products from "../components/mains/Product";
 
-import chair from '../imgs/chair.png'
-import chair2 from '../imgs/chair2.png'
-import chair3 from '../imgs/chair3.png'
-import chair4 from '../imgs/chair4.png'
-import desk from '../imgs/desk.png'
-import shelf from '../imgs/shelf.png'
-import room from '../imgs/room.jpg'
-import Goods from '../components/subcategories/goods'
+import chair from "../imgs/chair.png";
+import chair2 from "../imgs/chair2.png";
+import chair3 from "../imgs/chair3.png";
+import chair4 from "../imgs/chair4.png";
+import desk from "../imgs/desk.png";
+import shelf from "../imgs/shelf.png";
+import room from "../imgs/room.jpg";
+import Goods from "../components/subcategories/goods";
 
 const SubBlock = styled.div`
-    margin: 30px 40px;
-    div{
-        display: flex;
-    }
-    .sub-menus{
-        margin: 20px 0px;
-        justify-content: space-between;
-    }
+  margin: 210px 40px 30px 40px;
+  div {
+    display: flex;
+  }
+  .sub-menus {
+    margin: 20px 0px;
+    justify-content: space-between;
+  }
 `;
 
 const Sub = styled.div`
-    width: 200px;
-    height: 120px;
-    background-color: #F6F4E7;
-    &:hover{
-        background-color: #E1DFCE;
-    }
-    img {
-        width: 70px;
-        height: 70px;
-        margin-bottom: 7px;
-    }
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
+  width: 200px;
+  height: 120px;
+  background-color: #f6f4e7;
+  &:hover {
+    background-color: #e1dfce;
+  }
+  img {
+    width: 70px;
+    height: 70px;
+    margin-bottom: 7px;
+  }
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
 `;
 
 const ProductList = styled.div`
-    width: 100%;
-    flex-direction: column;
-    .total{
-        margin: 15px 0px;
-        font-weight: 600;
-        font-size: 20px;
-    }
-    .products{
-        flex-wrap : wrap
-    }
+  width: 100%;
+  flex-direction: column;
+  .total {
+    margin: 15px 0px;
+    font-weight: 600;
+    font-size: 20px;
+  }
+  .products {
+    flex-wrap: wrap;
+  }
 `;
 
 function SubCategory() {
@@ -60,46 +60,46 @@ function SubCategory() {
   useEffect(() => {
     Apis.get(`products`).then((data) => setProductList(data.data));
   }, []);
-//   console.log(productList);
+  //   console.log(productList);
 
-  return( 
+  return (
     <SubBlock>
-        <SubCarousel/>
-        <div className="sub-menus">
-            <Sub>
-                <img src={room}></img>
-                <div>전체보기</div>
-            </Sub>
-            <Sub>
-                <img src={desk}></img>
-                <div>책상</div>
-            </Sub>
-            <Sub>
-                <img src={shelf} alt='선반 카테고리'></img>
-                <div>선반</div>
-            </Sub>
-            <Sub>
-                <img src={chair}></img>
-                <div>의자</div>
-            </Sub>
+      <SubCarousel />
+      <div className="sub-menus">
+        <Sub>
+          <img src={room}></img>
+          <div>전체보기</div>
+        </Sub>
+        <Sub>
+          <img src={desk}></img>
+          <div>책상</div>
+        </Sub>
+        <Sub>
+          <img src={shelf} alt="선반 카테고리"></img>
+          <div>선반</div>
+        </Sub>
+        <Sub>
+          <img src={chair}></img>
+          <div>의자</div>
+        </Sub>
+      </div>
+      <ProductList>
+        <div className="total">0 개의 상품이 있습니다</div>
+        <div className="products">
+          {productList.map((product) => (
+            <Products
+              brand={product.brand}
+              img={product.img}
+              key={product.id}
+              name={product.name}
+              price={product.price}
+              star={product.star}
+            />
+          ))}
+          <Goods img={chair3} />
         </div>
-        <ProductList>
-            <div className="total">0 개의 상품이 있습니다</div>
-            <div className="products">
-            {productList.map((product) => (
-                <Products
-                   brand={product.brand}
-                   img={product.img}
-                   key={product.id}
-                   name={product.name}
-                   price={product.price}
-                   star={product.star}
-                />
-             ))}
-             <Goods img={chair3}/>
-            </div>
-        </ProductList>
+      </ProductList>
     </SubBlock>
-  )
+  );
 }
 export default SubCategory;
