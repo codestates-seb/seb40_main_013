9fd22bed65b61aadc6135e10657ab5496c5c1393
kkg5 <kkg05102@gmail.com>
2022-11-17 12:52:34 +0900

    :sparkles: feat: ReviewRepository > findByMember_Id 추가

--- server/src/main/java/gohome/dailydaily/domain/review/repository/ReviewRepository.java
+++ server/src/main/java/gohome/dailydaily/domain/review/repository/ReviewRepository.java
@@ -1,6 +1,8 @@
 package gohome.dailydaily.domain.review.repository;
 
 import gohome.dailydaily.domain.review.entity.Review;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -10,5 +12,7 @@ public interface ReviewRepository extends JpaRepository<Review, Long> {
 
     @EntityGraph(attributePaths = {"product", "member"})
     Optional<Review> findReviewById(Long reviewId);
-    
+
+    Page<Review> findByMember_Id(Long id, Pageable pageable);
+
 }
