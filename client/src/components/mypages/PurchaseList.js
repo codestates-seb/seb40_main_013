a9082429deef0692e9f7d258a8161d5174c5ed8a
devminoh <gmerit0116@naver.com>
2022-11-18 21:02:29 +0900

    :sparkles: feat: mypage 반응형, feature 수정 #29

--- client/src/components/mypages/PurchaseList.js
+++ client/src/components/mypages/PurchaseList.js
@@ -1,5 +1,5 @@
 import React from "react";
-import styled from "styled-components";
+import styled from "styled-components/macro";
 
 const Container = styled.form`
   display: flex;
@@ -8,6 +8,16 @@ const Container = styled.form`
   border-radius: 5px;
   padding: 20px 20px 20px 40px;
   width: 80%;
+  @media screen and (max-width: 390px){
+    width: 100%;
+    padding: 0 20px;
+    margin: 30px 0;
+  }
+  @media (min-width: 391px) and (max-width: 768px){
+    width: 100%;
+    padding: 0 20px;
+    margin: 30px 0;
+  }
 `;
 
 //상단
@@ -15,10 +25,17 @@ const Top = styled.div`
   display: flex;
   justify-content: space-between;
   margin: 5px 15px;
+  @media screen and (max-width: 390px){
+    margin: 3px 10px;
+  }
 `;
 const SubTop = styled.h2`
   font-weight: 700;
   font-size: 1.2rem;
+  @media screen and (max-width: 390px){
+    font-size: 12px;
+    font-weight: 600;
+  }
 `;
 
 const Hr = styled.hr`
@@ -42,26 +59,54 @@ const ProductStatus = styled.h2`
   font-weight: 700;
   font-size: 1.2rem;
   margin-right: 10px;
+  @media screen and (max-width: 390px){
+    font-size: 12px;
+    font-weight: 600;
+  }
 `;
 const DelieveryStatus = styled.h2`
   font-weight: 700;
   font-size: 1.2rem;
   color: #FFAF51;
+  @media screen and (max-width: 390px){
+    font-size: 12px;
+    font-weight: 600;
+  }
 `;
 
 const Detail = styled.div`
   display: flex;
   justify-content: space-between;
+  align-items: center;
   margin: 10px 20px;
+  @media screen and (max-width: 390px){
+    margin: 5px 10px;
+    justify-content: flex-start;
+  }
 `;
 const SubDetail = styled.div`
   display: flex;
+`;
+const ReactionSubDetail = styled.div`
+  display: flex;
+  @media screen and (max-width: 390px){
+    flex-direction: column;
+
+  }
 `;
 
 const Img = styled.img`
   width: 150px;
   height: 150px;
   margin-right: 10px;
+  @media screen and (max-width: 390px){
+    width: 60px;
+    height: 60px;
+  }
+  @media (min-width: 391px) and (max-width: 768px){
+    width: 100px;
+    height: 100px;
+  }
 `;
 const BP = styled.div`
   display: flex;
@@ -69,26 +114,103 @@ const BP = styled.div`
   justify-content: center;
   align-items: center;
   margin-left: 20px;
+  @media screen and (max-width: 390px){
+    flex-direction: row;
+    justify-content: flex-start;
+  }
 `;
 const BrandName = styled.h4`
   font-weight: 600;
   font-size: 1rem;
   margin-bottom: 5px;
+  @media screen and (max-width: 390px){
+    font-weight: 300;
+    font-size: 10px;
+    margin-right: 5px;
+  }
+  @media (min-width: 391px) and (max-width: 768px){
+    font-weight: 500;
+  }
 `;
 const ProductName = styled.h2`
   font-weight: 700;
   font-size: 1.3rem;
+  @media screen and (max-width: 390px){
+    font-weight: 500;
+    font-size: 12px;
+  }
+  @media (min-width: 391px) and (max-width: 768px){
+    font-weight: 600;
+    font-size: 1.2rem;
+  }
 `;
 const Price = styled.h2`
   margin: 10px 0;
+  @media screen and (max-width: 390px){
+    margin: 5px 5px 5px 0;
+    font-size: 12px;
+  }
+  @media (min-width: 391px) and (max-width: 768px){
+    margin: 5px 0;
+  }
 `;
 const Delievery = styled.h2`
   color: #FFAF51;
+  @media screen and (max-width: 390px){
+    font-size: 12px;
+  }
 `;
 const Quantity = styled.h2`
   display:flex;
   align-items: center;
   margin: 0 20px;
+  @media screen and (max-width: 390px){
+    font-size: 12px;
+    margin: 0 5px;
+  }
+`;
+const ReviewBtn = styled.button`
+  padding: 10px 50px;
+  height: 50px;
+  background-color: #002C6D;
+  color: white;
+  border-radius: 10px;
+  cursor: pointer;
+  &:hover{
+    background-color: #FFAF51;
+  }
+  @media screen and (max-width: 390px){
+    display: none;
+  }
+  @media (min-width: 391px) and (max-width: 768px){
+    padding: 8px 30px;
+  }
+`;
+
+//반응형 구매후기
+const ReactionSpace = styled.div`
+  display: none;
+    @media screen and (max-width: 390px){
+      display: flex;
+      justify-content: flex-end;
+      margin: 5px 10px;
+  }
+`;
+
+const ReactionReviewBtn = styled.button`
+  display: none;
+  @media screen and (max-width: 390px){
+    display: flex;
+    color: #515151;
+    border: 0.7px solid #AAAAAA;
+    border-radius: 5px;
+    padding: 3px 10px;
+    cursor: pointer;
+    &:hover{
+      background-color: #002C6D;
+      color: white;
+    }
+  }
 `;
 
 //브랜드 정보
@@ -99,15 +221,24 @@ const BrandDetail = styled.div`
   flex-direction: column;
   align-items: center;
   justify-content: center;
+  @media screen and (max-width: 390px){
+    height: 70px;
+  }
 `;
 const DelieveryWay = styled.div`
   color: rgba(81, 81, 81, 0.47);
   font-size: 1rem;
   margin-bottom: 10px;
+  @media screen and (max-width: 390px){
+    font-size: 12px;
+  }
 `;
 const BrandPhone = styled.div`
   font-weight: 800;
   font-size: 1.3rem;
+  @media screen and (max-width: 390px){
+    font-size: 15px;
+  }
 `;
 const PurchaseList = ()=>{
   return (
@@ -123,22 +254,26 @@ const PurchaseList = ()=>{
           <DelieveryStatus>도착완료</DelieveryStatus>
         </PD>
         <Detail>
-          <SubDetail>
-            <Img />
+          <Img />
+          <ReactionSubDetail>
             <BP>
               <BrandName>두닷</BrandName>
               <ProductName>화장대</ProductName>
             </BP>
-          </SubDetail>
-          <SubDetail>
-            <BP>
-              <ProductName>화장대</ProductName>
-              <Price>13900</Price>
-              <Delievery>일반배송</Delievery>
-            </BP>
-            <Quantity>1개</Quantity>
-          </SubDetail>
+            <SubDetail>
+              <BP>
+                {/* <ProductName>화장대</ProductName> */}
+                <Price>13900</Price>
+                <Delievery>일반배송</Delievery>
+              </BP>
+              <Quantity>1개</Quantity>
+            </SubDetail>
+          </ReactionSubDetail>
+          <ReviewBtn>리뷰작성</ReviewBtn>
         </Detail>
+        <ReactionSpace>
+          <ReactionReviewBtn>구매후기</ReactionReviewBtn>
+        </ReactionSpace>
       </Content>
       <BrandDetail>
         <DelieveryWay>무료배송</DelieveryWay>
