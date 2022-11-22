63ea8d2bf6a8c6cebea9d1ff5ae22d3482374f51
kkg5 <kkg05102@gmail.com>
2022-11-22 21:50:49 +0900

    :sparkles: feat: Redis 관련 Config 작성

---
+++ server/src/main/java/gohome/dailydaily/global/config/RedisRepositoryConfig.java
@@ -0,0 +1,25 @@
+package gohome.dailydaily.global.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
+
+@Configuration
+@EnableRedisRepositories
+public class RedisRepositoryConfig {
+
+    @Bean
+    public RedisConnectionFactory redisConnectionFactory() {
+        return new LettuceConnectionFactory();
+    }
+
+    @Bean
+    public RedisTemplate<?, ?> redisTemplate() {
+        RedisTemplate<?, ?> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setConnectionFactory(redisConnectionFactory());
+        return redisTemplate;
+    }
+}
