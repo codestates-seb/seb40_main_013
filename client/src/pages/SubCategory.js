29ea0970e5cfb8eb6d5ec5cb829ff6e8a0920137
insidelamp <rkdhfh@naver.com>
2022-11-23 11:06:45 +0900

    :bug: bug: merge 작업중

--- client/src/pages/SubCategory.js
+++ client/src/pages/SubCategory.js
@@ -11,16 +11,16 @@ import room from "../imgs/room.jpg";
 import axios from "axios";
 
 const SubBlock = styled.div`
-    width: 100%;
-    margin-top: 170px;
-    padding: 30px 40px 30px 40px;
-    div{
-        display: flex;
-    }
-    .sub-menus{
-        margin: 20px 0px;
-        justify-content: space-between;
-    }
+  width: 100%;
+  margin-top: 170px;
+  padding: 30px 40px 30px 40px;
+  div {
+    display: flex;
+  }
+  .sub-menus {
+    margin: 20px 0px;
+    justify-content: space-between;
+  }
 `;
 
 const Sub = styled.div`
@@ -53,37 +53,37 @@ const ProductList = styled.div`
   }
 `;
 
-function SubCategory({click}) {
-
+function SubCategory({ click }) {
   console.log(click);
 
   const [products, setProducts] = useState([]);
   const [page, setPage] = useState(0);
   const [loading, setLoading] = useState(false);
   const [prevY, setPrevY] = useState(0);
-  let productsRef = useRef({})
+  let productsRef = useRef({});
+  console.log(products);
 
   let loadingRef = useRef(null);
-   let prevYRef = useRef({});
-   let pageRef = useRef({});
-   productsRef.current = products;
-   pageRef.current = page;
+  let prevYRef = useRef({});
+  let pageRef = useRef({});
+  productsRef.current = products;
+  pageRef.current = page;
 
-   prevYRef.current = prevY
+  prevYRef.current = prevY;
 
   console.log(`loadingRef:`, loadingRef);
 
   useEffect(() => {
     getProducts();
     setPage(pageRef.current + 1);
-    
-    let options ={
-        root: null, //root는 기본적으로 스크롤 가능한 영역, null을 입력하면 전체 브라우저 창이 됨
-        rootMargin: '150px',
-        htreshold: 0.7 //관찰해야 하는 대상 요소의 100%를 의미한다.
-    }
 
-    const observer = new IntersectionObserver(handleObserver, options)
+    let options = {
+      root: null, //root는 기본적으로 스크롤 가능한 영역, null을 입력하면 전체 브라우저 창이 됨
+      rootMargin: "150px",
+      htreshold: 0.7, //관찰해야 하는 대상 요소의 100%를 의미한다.
+    };
+
+    const observer = new IntersectionObserver(handleObserver, options);
     observer.observe(loadingRef.current);
   }, [click]);
 
@@ -92,30 +92,31 @@ function SubCategory({click}) {
 
     const y = entities[0].boundingClientRect.y;
 
-    if (prevYRef.current > y){
-        console.log(`real get list`);
-        getProducts();
-        setPage(pageRef.current + 1);
+    if (prevYRef.current > y) {
+      console.log(`real get list`);
+      getProducts();
+      setPage(pageRef.current + 1);
     } else {
-        console.log('loading false');
+      console.log("loading false");
     }
     console.log(`currenty`, y, `prevY`, prevY);
     setPrevY(y);
-  }
-  
+  };
+
   const getProducts = async () => {
-    try{
-      let productsRes = await Apis.get(`products?main=${click}&page=${pageRef.current}`
+    try {
+      let productsRes = await Apis.get(
+        `products?main=${click}&page=${pageRef.current}`
       );
       if (productsRes) {
         setProducts([...productsRef.current, ...productsRes.data.content]);
         console.log(productsRes.data.content);
-        console.log(`page`,pageRef.current );
-      } 
+        console.log(`page`, pageRef.current);
+      }
     } catch (error) {
-        console.log('ERROR GETTING PRODUCTS');
+      console.log("ERROR GETTING PRODUCTS");
     }
-  }
+  };
 
   return (
     <SubBlock>
@@ -140,17 +141,9 @@ function SubCategory({click}) {
       </div>
       <ProductList>
         <div className="total">0 개의 상품이 있습니다</div>
-        <div className="products" >
-          {products?.map((product) => ( 
-            <Products
-              key={product.id}
-              brand={product.brand}
-              img={product.img}
-              title={product.title}
-              price={product.price}
-              score={product.score}
-              proId={product.id}
-            />
+        <div className="products">
+          {products?.map((product) => (
+            <Products key={product.id} product={product} proId={product.id} />
           ))}
         </div>
         <div ref={loadingRef}></div>
