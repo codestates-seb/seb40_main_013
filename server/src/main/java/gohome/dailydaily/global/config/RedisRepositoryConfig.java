c0fb85863473a3a2dc4f5b2b7a8e39cc9bf50c76
kkg5 <kkg05102@gmail.com>
2022-11-23 11:15:53 +0900

    :green_heart: chore: Redis Setting 수정

--- server/src/main/java/gohome/dailydaily/global/config/RedisRepositoryConfig.java
+++ server/src/main/java/gohome/dailydaily/global/config/RedisRepositoryConfig.java
@@ -1,5 +1,6 @@
 package gohome.dailydaily.global.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
@@ -10,10 +11,15 @@ import org.springframework.data.redis.repository.configuration.EnableRedisReposi
 @Configuration
 @EnableRedisRepositories
 public class RedisRepositoryConfig {
+    @Value("${spring.redis.host}")
+    private String redisHost;
+
+    @Value("${spring.redis.port}")
+    private int redisPort;
 
     @Bean
     public RedisConnectionFactory redisConnectionFactory() {
-        return new LettuceConnectionFactory();
+        return new LettuceConnectionFactory(redisHost, redisPort);
     }
 
     @Bean
