69247c3aa58a7414db360f93a7a9ce592d5f465c
JunYeol <wnsduf9160@naver.com>
2022-11-24 10:39:28 +0900

    refactor : product param dto, queryDsl 동적 할당 리팩토링

---
+++ server/src/main/java/gohome/dailydaily/domain/product/controller/dto/GetProductListByDto.java
@@ -0,0 +1,22 @@
+package gohome.dailydaily.domain.product.controller.dto;
+
+import gohome.dailydaily.global.common.dto.PagingRequestDto;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class GetProductListByDto extends PagingRequestDto {
+
+    private String main;
+    private String sub;
+    private Long sellerId;
+    private String title;
+
+    public GetProductListByDto(String main, String sub, Long sellerId, String title) {
+        this.main = main;
+        this.sub = sub;
+        this.sellerId = sellerId;
+        this.title = title;
+    }
+}
