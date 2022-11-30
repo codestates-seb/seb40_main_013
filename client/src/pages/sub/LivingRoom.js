73a22645496d9fc35f16e3879963473d096f2893
seoyeon <97490053+seoyeon0108@users.noreply.github.com>
2022-11-30 22:05:26 +0900

    [Client] 소분류 페이지 재수정 (#167)
    * Update feature-issues-default.md
    
    * :recycle: refactor: subcategory
    
    * :memo: feat: 카테고리별 페이지 분리
    
    * :art: style: 장바구니 반응형 적용 #122
    
    * :bug: pull
    
    * :sparkles: feat/search-result page #131
    
    * :sparkles: feat: 구매생성 구현
    
    * :bug: pull
    
    * :memo: refact: 최근검색어, 인기검색어 구현
    
    * :bug: pull error
    
    * :bug: pull
    
    * :bug: pull
    
    * :recycle: refactor: 소분류페이지 수정
    
    Co-authored-by: Hong SeungJae <107091097+Grooomit@users.noreply.github.com>

--- client/src/pages/sub/LivingRoom.js
+++ client/src/pages/sub/LivingRoom.js
@@ -2,57 +2,97 @@ import { useState, useEffect, useRef } from "react";
 import styled from "styled-components/macro";
 import SubCarousel from "../../components/subcategories/SubCalousel";
 import Products from "../../components/mains/Product";
-
 import { useDispatch, useSelector } from "react-redux";
-import { getLivingRoom } from "../../reduxstore/slices/sub/LivingroomSlice";
+import {getLibrary, getSub, getAsc, getCount} from "../../reduxstore/slices/sub/LibrarySlice";
+import RankingDown from "../../components/subcategories/DropDown";
 
-function LivingRoom({ click }) {
-  console.log(click);
+function LivingRoom({ mainClick, subclick }) {
+  
+  //소분류에 따른 대분류카테고리 이름 지정
+  let mainCateClick = '거실';
+  console.log(`mainCateClick`,mainCateClick,`subclick`,subclick);
 
   const dispatch = useDispatch();
-  const livingRoomSelector = useSelector(
-    (state) => state.livingroom.livingRoomInitial.content
-  ); 
-  console.log(livingRoomSelector);
+  const librarySelector = useSelector((state) => state.library.libraryInitial);
+  console.log('112', librarySelector);
+
+  // const subSelector = useSelector((state) => state.library);
+  // console.log(subSelector);
+
+  const countSelector = useSelector(
+    (state) => state.library.coutnInitial.count
+  );
 
   const [page, setPage] = useState(0);
   const [products, setProducts] = useState([]);
 
+  // 셀렉트 박스
+  const [dropDownclicked, setDropDownClicked] = useState("최신순");
+  const [third, setThird] = useState("desc");
+  const [closeDropDown, setDloseDropDown] = useState(false);
+
+  let sortArgument = "createdAt";
+  if(dropDownclicked ==='판매순'){
+    sortArgument = 'sale';
+  } else if(dropDownclicked === '높은가격순'|| dropDownclicked === '낮은가격순'){
+    sortArgument = 'price';
+  } else{
+    sortArgument = 'createdAt';
+  }
+
+
+  console.log(`dropDownclicked`, dropDownclicked,`sortArgument`,sortArgument, `third`, third );
+
+  const modalRef = useRef();
+
+  const closeHandler = () => {
+    setDloseDropDown(!closeDropDown);
+  };
+
+  const outModalCloseHandler = (e) => {
+    if (closeDropDown && !modalRef.current.contains(e.target))
+      setDloseDropDown(false);
+  };
   useEffect(() => {
-    dispatch(getLivingRoom({page })); //침실의 전체 
-}, []);
+    if (
+      subclick === '소파' ||
+      subclick === '거실장' ||
+      subclick === '수납장'
+    ) {
+      console.log(11);
+      dispatch(getSub({ mainCateClick, subclick, page, sortArgument, third }));
+    } else {
+      dispatch(getLibrary({ mainCateClick, page, sortArgument, third }));
+    }
+    // dispatch(getCount());
+  }, [subclick]);
 
-
-    return (
-      <SubBlock>
-        <SubCarousel />
-        <div className="sub-menus">
-          <Sub>
-            <div>전체보기</div>
-          </Sub>
-          <Sub>
-            <div>소파</div>
-          </Sub>
-          <Sub>
-            <div>거실장</div>
-          </Sub>
-          <Sub>
-            <div>수납장</div>
-          </Sub>
-        </div>
-        <FilterBlock>
-          <div className="total">0 개의 상품이 있습니다</div>
-          <div>최신순</div>
-        </FilterBlock>
-        <ProductList>
-            {livingRoomSelector?.map((product) => (
-              <Products proId={product.id} product={product} key={product.id} />
-            ))}
+  return (
+    <SubBlock onClick={outModalCloseHandler}>
+      <SubCarousel />
+      <FilterBlock>
+        <div className="total">{countSelector}개의 상품이 있습니다</div>
+        <section ref={modalRef}>
+          <RankingDown
+            dropDownclicked={dropDownclicked}
+            setDropDownClicked={setDropDownClicked}
+            closeDropDown={closeDropDown}
+            closeHandler={closeHandler}
+            setThird={setThird}
+            third={third}
+          />
+        </section>
+      </FilterBlock>
+      <ProductList>
+        {librarySelector?.map((product) => (
+          <Products proId={product.id} product={product} key={product.id} />
+        ))}
         {/* <div ref={loadingRef}></div> */}
       </ProductList>
     </SubBlock>
   );
 }
+
 export default LivingRoom;
 
 const SubBlock = styled.div`
@@ -76,22 +116,6 @@ const SubBlock = styled.div`
       display: flex;
       justify-content: flex-start;
     }
-`;
-
-const Sub = styled.div`
-    display: flex;
-    width: 13vw;
-    height: 6vh;
-    background-color: #fcf9e9;
-    margin: 0 1em;
-    &:hover {
-      background-color: #e1dfce;
-    }
-    color: #515151;
-    border-radius: 5px;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
 `;
 
 const FilterBlock = styled.div`
