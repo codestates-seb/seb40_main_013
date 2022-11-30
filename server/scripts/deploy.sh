f630cbae28eb2d0d2035e09e91304e91f8bff85a
Grooomit <tmd5972@gmail.com>
2022-11-30 21:40:39 +0900

    deploy: jar 파일을 server 환경으로 실행하도록 수정

--- server/scripts/deploy.sh
+++ server/scripts/deploy.sh
@@ -26,4 +26,4 @@ fi
 
 DEPLOY_JAR=$DEPLOY_PATH$JAR_NAME
 echo "> DEPLOY_JAR 배포"    >> /home/ec2-user/dailydaily/deploy.log
-sudo nohup java -jar $DEPLOY_JAR >> /home/ec2-user/deploy.log 2>/home/ec2-user/dailydaily/deploy_err.log &
+sudo nohup java -jar $DEPLOY_JAR --spring.profiles.active=server >> /home/ec2-user/deploy.log 2>/home/ec2-user/dailydaily/deploy_err.log &
