3babccaa62d70274a397237a0c219c607ff77d45
Grooomit <tmd5972@gmail.com>
2022-12-04 13:45:38 +0900

    fix: Custom Domain CORS 설정

--- server/src/main/java/gohome/dailydaily/global/common/security/config/SecurityConfig.java
+++ server/src/main/java/gohome/dailydaily/global/common/security/config/SecurityConfig.java
@@ -71,6 +71,7 @@ public class SecurityConfig {
         configuration.addAllowedOrigin("http://localhost:80");
         configuration.addAllowedOrigin("http://localhost");
         configuration.addAllowedOrigin("https://thunderous-kashata-a1b728.netlify.app/");
+        configuration.addAllowedOrigin("https://dailydaily.shop");
         configuration.addAllowedOrigin("http://seb40main013.s3-website.ap-northeast-2.amazonaws.com/");
         configuration.addAllowedOrigin("http://ec2-3-39-250-169.ap-northeast-2.compute.amazonaws.com/");
         configuration.setAllowedMethods(List.of("GET", "POST", "PATCH", "PUT", "DELETE", "OPTIONS"));
