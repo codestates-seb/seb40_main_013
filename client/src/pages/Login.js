316621036a4069149b7c4a85b0c04e1b8bd63c25
insidelamp <rkdhfh@naver.com>
2023-01-10 23:45:13 +0900

    :recycle: refactor: 이미지 지연로딩 및 관리자 로그인 이동 및  말풍선추가

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -6,26 +6,35 @@ import { loginUser } from "../reduxstore/slices/userSlice";
 import loginImg from "../imgs/chairImage.png";
 import { AiOutlineCheckCircle } from "react-icons/ai";
 import { guestUser } from "../reduxstore/slices/userSlice";
+import { IoMdPerson } from "react-icons/io";
 
 function Login() {
   const [userWriteEmail, setUserWriteEmail] = useState("");
   const [userWritePwd, setUserWritePwd] = useState("");
   const [userWriteInput, setUserWriteInput] = useState(false);
   const [userStateCheck, setUserStateCheck] = useState(false);
+
   const navigate = useNavigate();
   const dispatch = useDispatch();
   const routeSignup = () => {
     navigate("/signup");
   };
+
   const writeChangeEmail = (e) => {
     setUserWriteEmail(e.target.value);
   };
+
   const writeChangePwd = (e) => {
     setUserWritePwd(e.target.value);
   };
 
   const clickState = () => {
     setUserStateCheck(!userStateCheck);
+  };
+
+  const clickGuest = (e) => {
+    e.preventDefault();
+    dispatch(guestUser({ navigate }));
   };
 
   const clickLogin = (e) => {
@@ -80,11 +89,13 @@ function Login() {
             />
           </LoginInputSpace>
           <LoginButton onClick={clickLogin}>로그인</LoginButton>
+          <LoginButton onClick={clickGuest} className="adminLogin">
+            관리자 로그인
+          </LoginButton>
+
           <LoginInformationSpace>
-            <LoginCheckSpace isCheck={userStateCheck}>
-              <LoginState onClick={clickState}>
-                <AiOutlineCheckCircle />
-              </LoginState>
+            <LoginCheckSpace isCheck={userStateCheck} onClick={clickState}>
+              <LoginState />
               <LoginStateContent>로그인 상태 유지</LoginStateContent>
             </LoginCheckSpace>
           </LoginInformationSpace>
@@ -94,6 +105,12 @@ function Login() {
           </LoginRouteSign>
         </LoginContentSpace>
       </LoginWrapper>
+      <Bubble>
+        <BubbleContents>
+          관리자 로그인 버튼을 누르면 <br /> 아이디, 비번 입력 없이 로그인
+          가능합니다
+        </BubbleContents>
+      </Bubble>
     </Wrapper>
   );
 }
@@ -121,6 +138,7 @@ const LoginWrapper = styled.div`
   border: 1px solid var(--color-gray);
   display: flex;
   border-radius: 7px;
+  position: relative;
 
   @media screen and (max-width: 500px) {
     min-width: 240px;
@@ -174,6 +192,7 @@ const LoginTitle = styled.div`
     font-size: 30px;
   }
 `;
+
 const LoginInputSpace = styled.div`
   width: 90%;
   display: flex;
@@ -181,6 +200,7 @@ const LoginInputSpace = styled.div`
   align-items: center;
   margin-top: 20px;
 `;
+
 const LoginInput = styled.input`
   width: 85%;
   height: 60px;
@@ -201,13 +221,21 @@ const LoginInput = styled.input`
 const LoginButton = styled.button`
   width: 77%;
   height: 45px;
+  border: 1px solid var(--color-navy);
   background-color: var(--color-navy);
   color: white;
   font-size: 20px;
-  margin-top: 40px;
+  margin-top: 15px;
   border-radius: 5px;
   &:hover {
     cursor: pointer;
+  }
+  &.adminLogin {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    background-color: white;
+    color: var(--color-navy);
   }
 `;
 
@@ -217,7 +245,9 @@ const LoginInformationSpace = styled.div`
   height: 40px;
   font-size: 18px;
   color: var(--color-gray);
+  cursor: pointer;
 `;
+
 const LoginCheckSpace = styled.div`
   width: 100%;
   display: flex;
@@ -226,11 +256,23 @@ const LoginCheckSpace = styled.div`
   color: ${(state) => (state.isCheck ? "#FFAF51" : "#AAAAAA")};
   margin-top: 10px;
 `;
-const LoginState = styled.div`
-  margin-right: 10px;
+
+const LoginState = styled(AiOutlineCheckCircle)`
+  margin-right: 5px;
+  margin-top: -1.5px;
 `;
+
 const LoginStateContent = styled.div`
-  font-size: 17px;
+  font-size: 16px;
+  @media screen and (max-width: 380px) {
+    font-size: 12px;
+  }
+  @media (min-width: 381px) and (max-width: 767px) {
+    font-size: 14px;
+  }
+  @media screen and (min-width: 1024px) {
+    font-size: 15px;
+  }
 `;
 
 const LoginRouteSign = styled.div`
@@ -243,7 +285,10 @@ const LoginRouteSign = styled.div`
   font-weight: 500;
   margin-top: 20px;
   margin-bottom: 20px;
-  font-size: 12px;
+  font-size: 13px;
+  @media (min-width: 1023px) {
+    font-size: 15px;
+  }
 `;
 
 const LoginSignBtn = styled.div`
@@ -254,6 +299,54 @@ const LoginSignBtn = styled.div`
   cursor: pointer;
   &:hover {
     color: var(--color-navy);
+  }
+`;
+const Bubble = styled.div`
+  width: 20%;
+  height: 15%;
+  position: relative;
+  background: #ffffff;
+  border: 4px solid var(--color-navy);
+  border-radius: 15px;
+  top: 55%;
+  left: 30px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  &::after,
+  &::before {
+    right: 100%;
+    top: 50%;
+    border: solid transparent;
+    content: "";
+    height: 0;
+    width: 0;
+    position: absolute;
+    pointer-events: none;
+  }
+  &::after {
+    border-color: rgba(255, 255, 255, 0);
+    border-right-color: #ffffff;
+    border-width: 25px;
+    margin-top: -25px;
+  }
+  &::before {
+    border-color: rgba(24, 0, 245, 0);
+    border-right-color: var(--color-navy);
+    border-width: 31px;
+    margin-top: -31px;
+  }
+  @media screen and (max-width: 768px) {
+    top: -5%;
+    left: 30px;
+  }
+`;
+
+const BubbleContents = styled.p`
+  color: var(--color-navy);
+  font-size: 0.9vw;
+  @media screen and (max-width: 768px) {
+    font-size: 0.6vw;
   }
 `;
 
